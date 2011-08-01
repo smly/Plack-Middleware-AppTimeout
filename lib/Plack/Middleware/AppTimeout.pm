dc0788ab6ca23962d87de5edc7ad174a34984798
smly <eowner@gmail.com>
2011-08-02 00:17:20 +0900

    bump version

--- lib/Plack/Middleware/AppTimeout.pm
+++ lib/Plack/Middleware/AppTimeout.pm
@@ -9,7 +9,7 @@ use Plack::Util::Accessor qw(
     app_timeout_sec
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.04';
 
 sub prepare_app {
     my $self = shift;
@@ -19,17 +19,37 @@ sub call {
     my ($self, $env) = @_;
 
     my $timeout = $self->app_timeout_sec || 300;
-    my $res;
+    my $res = undef;
+    my $flag = 0;
     eval {
-        local $SIG{ALRM} = sub { die };
+        local $SIG{ALRM} = sub { $flag = 1; die };
         alarm($timeout);
         $res = $self->app->($env);
-        my $timeleft = alarm(0);
+        alarm(0);
+    };
+    alarm(0);
+
+    if ($@) {
+        if (!$flag) {
+            # handling application error
+            $res = [ 500, [ 'Content-Type' => 'text/plain' ], [ 'Internal Server Error.' ] ];
+        } else {
+            # handling application timeout
+            $res = [ 503, [ 'Content-Type' => 'text/plain' ], [ 'Service Temporarily Unavailable' ] ];
+        }
+    }
+
+    # validate response code
+    eval {
+        if ($res->[0] !~ /^\d{3}/) {
+            # invalid response code
+            $res = [ 500, [ 'Content-Type' => 'text/plain' ], [ 'Internal Server Error.' ] ];
+        }
     };
 
     if ($@) {
-        # TODO: handling user specfied 503 page
-        $res = [ 503, [ 'Content-Type' => 'text/plain' ], [ '503' ] ];
+        # no response code
+        $res = [ 500, [ 'Content-Type' => 'text/plain' ], [ 'Internal Server Error.' ] ];
     }
 
     return $res;
